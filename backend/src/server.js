1eb443da89bb040b67095f37ea869504fc13472b
sankarkarmakar01 <karmakarsankar000@gmail.com>
2026-02-14 21:18:46 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoute from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoute);
+app.use("/api/messages", messageRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
