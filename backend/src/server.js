6c90808d6cf1f8e19683b8abb8c83d603ccc6e7c
sankarkarmakar01 <karmakarsankar000@gmail.com>
2026-02-16 08:38:09 +0530

    Commit for Auth Middleware & Update Profile

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cookieParser from 'cookie-parser'
 
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -15,6 +16,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json())  //req.body
+app.use(cookieParser())
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
