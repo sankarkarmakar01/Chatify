73058f265032cf4e44625ac7a719a02dfbed773d
sankarkarmakar01 <karmakarsankar000@gmail.com>
2026-02-24 10:10:56 +0530

    Commit for message endpoints

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,20 @@
 import express from "express";
+import {
+  getAllContects,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-  res.send("Send");
-});
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContects);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
